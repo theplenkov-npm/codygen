22d32ed74b256da18181bb872aba4343e142b1e3
Petr Plenkov <petr.plenkov@gmail.com>
2025-02-11 09:57:09 +0000

    codygen: initial version

--- packages/codygen/src/index.ts
+++ packages/codygen/src/index.ts
@@ -1,1 +1,16 @@
-export * from './lib/codygen';
+import { program } from '@commander-js/extra-typings';
+
+// codygen CLI
+// Usage: Echo "Hello, World ts app" | codygen
+import { codygen } from './lib/codygen.js';
+
+program
+  .name('codygen')
+  .description('Sourcegraph Cody wrapper for generating code from prompts')
+  .option('-o,--output <output>', 'Output folder')
+  .option('-c, --context <context...>')
+  .action(({ output, context }) => {
+    codygen({ output, context });
+  });
+
+program.parseAsync(process.argv);
---
+++ packages/codygen/src/lib/parser.ts
@@ -0,0 +1,35 @@
+// cody output parser
+
+interface Snippet {
+  filename: string;
+  content: string;
+}
+
+const snippetStartRegex = /^```[a-zA-Z]*:(.*)$/;
+const snippetEndRegex = /^```$/;
+
+export function parseChatOutput(input: string): Array<Snippet> {
+  const snippets: Snippet[] = [];
+
+  var currentFile: Snippet | undefined;
+
+  for (const line of input.split('\n')) {
+    if (!currentFile && snippetStartRegex.test(line)) {
+      currentFile = {
+        filename: line.match(snippetStartRegex)![1],
+        content: '',
+      };
+    }
+
+    if (currentFile && snippetEndRegex.test(line)) {
+      snippets.push(currentFile);
+      currentFile = undefined;
+    }
+
+    if (currentFile && !snippetStartRegex.test(line)) {
+      currentFile.content += line + '\n';
+    }
+  }
+
+  return snippets;
+}
